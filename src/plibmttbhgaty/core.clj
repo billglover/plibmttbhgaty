80827bc544b5100f6f5d5832c47b79b94ca4119f
Bill Glover <bill@billglover.co.uk>
2016-07-02 16:53:25 +0100

    The classic fizzBuzz

--- src/plibmttbhgaty/core.clj
+++ src/plibmttbhgaty/core.clj
@@ -1,7 +1,21 @@
 (ns plibmttbhgaty.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn divBy [x y]
+	(if	( = (mod y x) 0) true false))
+
+(defn fizzBuzz [x]
+	(if (= 0 x) 0
+		(if	(divBy 15 x)
+			"fizzBuzz" 
+			(if	(divBy 5 x) 
+				"buzz" 
+				(if (divBy 3 x) 
+					"fizz"
+					x )))))
+
+(defn -main [& args]
+  (doall 
+  	(for [x (map fizzBuzz (take 20 (range)))]
+	(println x))))
+
